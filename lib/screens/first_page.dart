04e64f0052454652cccb34124696fcdff1a9eae1
Moemen1001 <moemenmontsr18@gmail.com>
2024-05-01 21:50:38 +0300

    basic code done

--- lib/screens/first_page.dart
+++ lib/screens/first_page.dart
@@ -1,2 +1,29 @@
 import 'package:flutter/material.dart';
-class firt extends StatefulWidget;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Login Page'),
+      ),
+      body: const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [],
+      ),
+    ));
+  }
+}
